ca3df318f15b072d037eb89e6b267eaf2a43dbe0
collegegate2580 <collegegatee@gmail.com>
2025-06-09 11:47:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def save_chat(user_input, reply):
     }
     chat_collection.insert_one(chat_data)
 
-@app.route("https://cg-gate-chatbot-1.onrender.com/chat", methods=["POST"])
+@app.route("https://cg-gate-chatbot-3.onrender.com/chat", methods=["POST"])
 def chat():
     user_input = request.json.get("message")
     print(f"Received user input: {user_input}")  # Debugging log
